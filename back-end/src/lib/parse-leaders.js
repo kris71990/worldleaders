f6efbbfebd6cdbecaf3d34b705808b6d20e40679
Kris Sakarias <ksakarias@gmail.com>
2018-07-15 15:07:35 -0700

    moving leader parsing to backend

--- back-end/src/lib/parse-leaders.js
+++ back-end/src/lib/parse-leaders.js
@@ -1,3 +1,55 @@
 'use strict';
 
 import logger from './logger';
+
+if (selected.headOfGovernmentFull && selected.chiefOfStateFull) {
+  const fullGov = selected.headOfGovernmentFull;
+  const fullState = selected.chiefOfStateFull;
+  const multsGov = fullGov.includes(';') ? fullGov.split('; ') : null;
+  const multsState = fullState.includes(';') ? fullState.split(';') : null;
+  
+  const keywordsGov = {};
+  if (multsGov) {
+    multsGov.map((x, i) => {
+      const split = multsGov[i].split(' ');
+      const root = multsGov[i].match(/\b[A-Z]+\b/g);
+      if (!root) return null;
+      if (root.length === 1) {
+        const main = split.indexOf(root[0]);
+        keywordsGov[i + 1] = split.slice(0, main + 1);
+      } else {
+        root.forEach((y) => {
+          console.log(multsGov[i].split(' ').indexOf(y));
+        });
+      }
+      return null;
+    });
+  } else {
+    const split = fullGov.split(' ');
+    const root = fullGov.match(/\b[A-Z]+\b/g);
+
+    if (root) {
+      if (root.length === 1) {
+        const main = split.indexOf(root[0]);
+        keywordsGov[1] = split.slice(0, main + 1);
+      } else {
+        console.log('more than one capitalized word');
+      }
+    }
+  }
+  console.log(keywordsGov);
+
+  // const keywordsState = {};
+  // if (multsState) {
+  //   multsState.map((x, i) => {
+  //     keywordsState[i] = multsState[i].match(/\b[A-Z]+\b/g);
+  //     let ind = multsState[i].indexOf(keywordsState[i]);
+  //     // console.log(ind);
+  //     return null;
+  //   });
+  // } else {
+  //   keywordsState[1] = fullState.match(/\b[A-Z]+\b/g);
+  //   let ind = fullState.indexOf(keywordsState[1]);
+  // console.log(ind);
+  // }
+}
--- front-end/src/components/system/system.js
+++ front-end/src/components/system/system.js
@@ -46,47 +46,6 @@ class System extends React.Component {
         capitalJSX = <p>{selected.capital}</p>;
       }
     }
-    if (selected.headOfGovernmentFull && selected.chiefOfStateFull) {
-      const fullGov = selected.headOfGovernmentFull;
-      const fullState = selected.chiefOfStateFull;
-      const multsGov = fullGov.includes(';') ? fullGov.split(';') : null;
-      const multsState = fullState.includes(';') ? fullState.split(';') : null;
-      
-      const keywordsGov = {};
-      if (multsGov) {
-        multsGov.map((x, i) => {
-          const caps = multsGov[i].match(/\b[A-Z]+\b/g);
-          keywordsGov[i + 1] = caps;
-          return null;
-        });
-      } else {
-        keywordsGov[1] = fullGov.match(/\b[A-Z]+\b/g);
-      }
-
-      const keywordsState = {};
-      if (multsState) {
-        multsState.map((x, i) => {
-          const caps = multsState[i].match(/\b[A-Z]+\b/g);
-          keywordsState[i + 1] = caps;
-          return null;
-        });
-      } else {
-        keywordsState[1] = fullState.match(/\b[A-Z]+\b/g);
-      }
-
-      console.log('head of gov', keywordsGov);
-      console.log('chief of state', keywordsState);
-      // const hogs = selected.headOfGovernmentFull.match(/\b[A-Z]+\b/g);
-      // const hos = selected.chiefOfStateFull.match(/\b[A-Z]+\b/g);
-      // const split = selected.headOfGovernmentFull.split(' ');
-      // console.log(split);
-
-      // const full = hogs.map((x) => {
-      //   console.log(x);
-      //   return split.indexOf(x);
-      // });
-      // console.log(full, hogs);
-    }
 
     return (
       <div className="system-info">
