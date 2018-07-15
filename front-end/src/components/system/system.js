2a365ae13eede9c19d7074c6a6b955b6d9f301a4
Kris Sakarias <ksakarias@gmail.com>
2018-07-15 13:33:39 -0700

    frontend parsing of political figures

--- front-end/src/components/system/system.js
+++ front-end/src/components/system/system.js
@@ -46,26 +46,55 @@ class System extends React.Component {
         capitalJSX = <p>{selected.capital}</p>;
       }
     }
-    // if (selected.headOfGovernment && selected.chiefOfState) {
-    //   const hogs = selected.headOfGovernment.match(/\b[A-Z]+\b/g);
-    //   const hos = selected.chiefOfState.match(/\b[A-Z]+\b/g);
-    //   const split = selected.headOfGovernment.split(' ');
-    //   console.log(split);
+    if (selected.headOfGovernmentFull && selected.chiefOfStateFull) {
+      const fullGov = selected.headOfGovernmentFull;
+      const fullState = selected.chiefOfStateFull;
+      const multsGov = fullGov.includes(';') ? fullGov.split(';') : null;
+      const multsState = fullState.includes(';') ? fullState.split(';') : null;
+      
+      const keywordsGov = {};
+      if (multsGov) {
+        multsGov.map((x, i) => {
+          const caps = multsGov[i].match(/\b[A-Z]+\b/g);
+          keywordsGov[i + 1] = caps;
+          return null;
+        });
+      } else {
+        keywordsGov[1] = fullGov.match(/\b[A-Z]+\b/g);
+      }
 
-    //   const full = hogs.map((x) => {
-    //     console.log(x);
-    //     return split.indexOf(x);
-    //   });
-    //   console.log(full, hogs);
-    // }
+      const keywordsState = {};
+      if (multsState) {
+        multsState.map((x, i) => {
+          const caps = multsState[i].match(/\b[A-Z]+\b/g);
+          keywordsState[i + 1] = caps;
+          return null;
+        });
+      } else {
+        keywordsState[1] = fullState.match(/\b[A-Z]+\b/g);
+      }
+
+      console.log('head of gov', keywordsGov);
+      console.log('chief of state', keywordsState);
+      // const hogs = selected.headOfGovernmentFull.match(/\b[A-Z]+\b/g);
+      // const hos = selected.chiefOfStateFull.match(/\b[A-Z]+\b/g);
+      // const split = selected.headOfGovernmentFull.split(' ');
+      // console.log(split);
+
+      // const full = hogs.map((x) => {
+      //   console.log(x);
+      //   return split.indexOf(x);
+      // });
+      // console.log(full, hogs);
+    }
 
     return (
       <div className="system-info">
         <h1>{selected.fullName}</h1><br/>
         <h4>Type of Government: <span>{selected.typeOfGovernment}</span></h4><br/>
         <h4>Capital: </h4>{capitalJSX}<br/>
-        <p>{selected.chiefOfState}</p><br/>
-        <p>{selected.headOfGovernment}</p><br/>
+        <p>{selected.chiefOfStateFull}</p><br/>
+        <p>{selected.headOfGovernmentFull}</p><br/>
         {
           selected.independence ? 
             <h4>Independence: <span>{selected.independence}</span></h4>
