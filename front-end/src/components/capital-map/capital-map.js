73cf849d0d005be8b8667ff690f7f6d68af08305
Kris Sakarias <ksakarias@gmail.com>
2018-07-24 13:17:46 -0700

    rendering google map

--- front-end/src/components/capital-map/capital-map.js
+++ front-end/src/components/capital-map/capital-map.js
@@ -1,35 +1,69 @@
 import React from 'react';
+import { compose, withProps } from 'recompose';
+import { 
+  withScriptjs, 
+  withGoogleMap, 
+  GoogleMap, 
+  Marker, 
+} from 'react-google-maps';
 import PropTypes from 'prop-types';
-
-import * as routes from '../../utils/routes';
-import * as systemActions from '../../actions/systemActions';
-import * as countryActions from '../../actions/countryActions';
-import FlagForm from '../flagForm/flag-form';
 
 import './capital-map.scss';
 
-class CapitalMap extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      error: null,
-    };
+const MyMapComponent = compose(
+  withProps({
+    googleMapURL: `https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}`,
+    loadingElement: <div style={{ height: '80%' }} />,
+    containerElement: <div style={{ height: '300px' }} />,
+    mapElement: <div style={{ height: '100%' }} />,
+  }),
+  withScriptjs,
+  withGoogleMap,
+)(props => <GoogleMap
+    defaultZoom={8}
+    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+  >
+  {
+    props.isMarkerShown && 
+    <Marker position={{ lat: -34.397, lng: 150.644 }} 
+    onClick={props.onMarkerClick} 
+    />
+  }
+  </GoogleMap>,
+);
+
+class CapitalMap extends React.PureComponent {
+  state = {
+    isMarkerShown: false,
   }
 
   componentDidMount() {
-    fetch(`https://maps.googleapis.com/maps/api/js?key=${process.env.GOOGLE_API_KEY}&callback=initMap`)
+    this.delayedShowMarker();
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true });
+    }, 3000);
+  }
+
+  handleMarkerClick = () => {
+    this.setState({ isMarkerShown: false });
+    this.delayedShowMarker();
   }
 
   render() {
     return (
-      <div className="google-map">
-      </div>
+      <MyMapComponent
+        isMarkerShown={this.state.isMarkerShown}
+        onMarkerClick={this.handleMarkerClick}
+      />
     );
   }
 }
 
 CapitalMap.propTypes = {
   selected: PropTypes.object,
-}
+};
 
 export default CapitalMap;
