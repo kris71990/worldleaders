caa1e6686e479923197d508e013e99f4ce8d6994
Kris Sakarias <ksakarias@gmail.com>
2018-07-26 17:13:14 -0700

    finished validation middleware

--- back-end/src/routes/country-router.js
+++ back-end/src/routes/country-router.js
@@ -10,17 +10,12 @@ import System from '../models/gov-system';
 import logger from '../lib/logger';
 import data from '../../data.json';
 
-const util = require('util');
-
-
 const jsonParser = json();
 const countryRouter = new Router();
 
 // returns posted country json
 countryRouter.post('/countries', jsonParser, countryParser, (request, response, next) => {
   logger.log(logger.INFO, `Processing a ${request.method} on ${request.url}`);
-
-  console.log(util.inspect(Object.keys(data.countries), { maxArrayLength: null }));
 
   const searchableCountry = request.body.countryName.replace(/([\s]+)/g, '_').toLowerCase();
 
